0100602c32d903e07d009e38b7b04b4fa378db71
AdamN24 <110106952+AdamN24@users.noreply.github.com>
2022-07-27 15:15:31 +0200

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,3 +1,4 @@
 --[[
-Hello, there you can find all roblox related scripts for a public use, have fun!
+Hello, there you can find all roblox related scripts for a public use, all projects are developed in mere hours or minutes, so don't expect anything big.
+P.S don't steal.
 --]]
